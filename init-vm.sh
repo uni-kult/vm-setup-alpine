16b0eaeac69c148a0a6847636cc235fbba42774f
uni-kult <147112152+uni-kult@users.noreply.github.com>
2025-05-08 10:51:55 +0200

    Update init-vm.sh

--- init-vm.sh
+++ init-vm.sh
@@ -10,8 +10,7 @@ fi
 NAME="$1"
 IP="$2"
 RESTIC_PASSWORD="$3"
-
-rm /etc/ssh/ssh_host_* && ssh-keygen -A
+RANDOM_STRING="$4"
 
 echo Name: $NAME
 echo IP: $IP
@@ -33,4 +32,33 @@ EOF
 rc-service networking restart
 
 apk update && apk upgrade
-wget -O /var/tmp/tempfile http://speedtest.belwue.net/random-100M >/dev/null 2>&1 && find / -size +1k >/dev/null 2>&1 && ls -R / >/dev/null 2>&1 && rm /var/tmp/tempfile >/dev/null 2>&1 && sync # increase entropy
+
+######### start of: increase entropy
+wget -O /var/tmp/tempfile http://speedtest.belwue.net/random-100M >/dev/null 2>&1 && find / -size +1k >/dev/null 2>&1 && ls -R / >/dev/null 2>&1 && rm /var/tmp/tempfile >/dev/null 2>&1 && sync
+ENTROPY_INPUT=$(mktemp)
+curl -s "https://www.random.org/cgi-bin/randbyte?nbytes=1024&format=h" >> "$ENTROPY_INPUT" || true
+curl -s "https://beacon.nist.gov/beacon/2.0/pulse/last" >> "$ENTROPY_INPUT" || true
+{
+    hostname
+    date
+    cat /proc/interrupts
+    cat /proc/net/dev
+    cat /proc/diskstats
+    cat /proc/meminfo
+    cat /proc/stat
+    ip addr
+    ps aux
+    echo $RANDOM_STRING
+} >> "$ENTROPY_INPUT"
+sha256sum "$ENTROPY_INPUT" > /dev/random
+rm "$ENTROPY_INPUT"
+######### end of: increase entropy
+
+rm '/etc/ssh/ssh_host_ecdsa_key'
+rm '/etc/ssh/ssh_host_ecdsa_key.pub'
+rm '/etc/ssh/ssh_host_ed25519_key'
+rm '/etc/ssh/ssh_host_ed25519_key.pub'
+rm '/etc/ssh/ssh_host_rsa_key'
+rm '/etc/ssh/ssh_host_rsa_key.pub'
+
+ssh-keygen -A
