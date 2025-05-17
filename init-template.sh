8d61e7e5e9c07fb92ca2a67832862d32279ae94f
uni-kult <147112152+uni-kult@users.noreply.github.com>
2025-05-17 08:29:33 +0200

    Update init-template.sh

--- init-template.sh
+++ init-template.sh
@@ -15,7 +15,7 @@ rc-update add qemu-guest-agent
 adduser user -G users -DH -u 1000 -s /sbin/nologin # reserve UID 1000
 wget -O /var/tmp/tempfile http://speedtest.belwue.net/random-100M >/dev/null 2>&1 && find / -size +1k >/dev/null 2>&1 && ls -R / >/dev/null 2>&1 && rm /var/tmp/tempfile >/dev/null 2>&1 && sync # increase entropy
 echo "41 3 * * * apk update && apk upgrade" | tee -a /var/spool/cron/root > /dev/null
-apk add micro tmux curl wget htop mosh rsync iputils-ping bat exa rng-tools
+apk add micro tmux curl wget htop mosh rsync iputils-ping bat exa rng-tools lazydocker ctop cfdisk e2fsprogs-extra
 
 apk add audit
 apk add ip6tables ufw
